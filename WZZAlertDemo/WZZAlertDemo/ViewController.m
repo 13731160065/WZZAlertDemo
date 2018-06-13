eaefef0a1def78d6eed70403937e669743e2c06f
13731160065 <714318973@qq.com>
2018-06-13 10:51:20 +0800

    f

--- WZZAlertDemo/WZZAlertDemo/ViewController.m
+++ WZZAlertDemo/WZZAlertDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "WZZAlertView.h"
 
 @interface ViewController ()
 
@@ -16,14 +17,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    WZZAlertAction * action = [WZZAlertAction actionWithTitle:@"快速创建" action:^(WZZAlertView *aAlertView) {
+        [aAlertView dismiss];
+        
+        //一个快速创建的按钮
+        [[WZZAlertView alertWithTitle:@"快速创建" detail:@"快速创建一个按钮的弹框" actions:nil] show];
+        
+    }];
+    
+    WZZAlertAction * action2 = [WZZAlertAction actionWithTitle:@"一个按钮" action:^(WZZAlertView *aAlertView) {
+        [aAlertView dismiss];
+        
+        //一个按钮示范
+        WZZAlertAction * action1 = [WZZAlertAction actionWithTitle:@"好的" action:^(WZZAlertView *aAlertView) {
+            [aAlertView dismiss];
+        }];
+        WZZAlertView * alertView2 = [WZZAlertView alertWithTitle:@"一个按钮" detail:@"自定义一个按钮文字的弹框" actions:@[action1]];
+        [alertView2 show];
+        
+    }];
+    
+    WZZAlertAction * action3 = [WZZAlertAction actionWithTitle:@"两个按钮" action:^(WZZAlertView *aAlertView) {
+        [aAlertView dismiss];
+        
+        //两个按钮效果
+        WZZAlertAction * action1 = [WZZAlertAction actionWithTitle:@"好的" action:^(WZZAlertView *aAlertView) {
+            [aAlertView dismiss];
+        }];
+        WZZAlertAction * action2 = [WZZAlertAction actionWithTitle:@"好的" action:^(WZZAlertView *aAlertView) {
+            [aAlertView dismiss];
+        }];
+        WZZAlertView * alert2 = [WZZAlertView alertWithTitle:@"一个按钮" detail:@"自定义两个按钮文字的弹框" actions:@[action1, action2]];
+        [alert2 show];
+        
+    }];
+    
+    WZZAlertView * alert = [WZZAlertView alertWithTitle:@"多个按钮" detail:@"自定义两个按钮文字的弹框" actions:@[action, action2, action3]];
+    [alert show];
 }
-
 
 @end
